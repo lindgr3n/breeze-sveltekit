5e328a13040d6a9a5fe91dd9accb2a96372184af
Johan Lindgren <johan@lindgr3n.com>
2022-02-18 21:40:11 +0100

    feat: tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,35 +1,39 @@
 <script context="module">
-    export async function load({ url, session }) {
-        const {authenticated, guest} = session;
-        
-        // If we are not on a guest endpoint and we are not authenticated we redirect
-        if(!authenticated && !guest) {
-            return {
-			    status: 302,
-			    redirect: '/login'
-		    };
-        }
+	export async function load({ url, session }) {
+		const { authenticated, guest } = session;
 
-        return {
+		// If we are not on a guest endpoint and we are not authenticated we redirect
+		if (!authenticated && !guest) {
+			return {
+				status: 302,
+				redirect: '/login'
+			};
+		}
+
+		return {
 			status: 200,
 			props: {
 				// user: response ? response.data : {}
 				user: {}
 			}
 		};
-    }
+	}
+</script>
+
+<script>
+	import '../app.css';
 </script>
 
 <div class="min-h-screen bg-gray-100">
-    <!-- <Navigation user={user} /> -->
+	<!-- <Navigation user={user} /> -->
 
-    <!-- Page Heading -->
-    <!-- <header class="bg-white shadow">
+	<!-- Page Heading -->
+	<!-- <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {{header}}
         </div>
     </header> -->
 
-    <!-- Page Content -->
-    <main><slot /></main>
+	<!-- Page Content -->
+	<main><slot /></main>
 </div>
