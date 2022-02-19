a8f0c982086809328107b2466ce3d17e66d0d35d
Johan Lindgren <johan@lindgr3n.com>
2022-02-19 16:06:01 +0100

    feat: tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: 'media',
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
-	plugins: []
+	plugins: [require('@tailwindcss/forms')]
 };
 
 module.exports = config;
